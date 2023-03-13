f0fcb7d9581bfa458c6cb9679fc82ced06e41da8
William Droz <william.droz@idiap.ch>
2023-03-13 09:29:26 +0100

    chore: basic argparse for main

--- activity_predictor/main.py
+++ activity_predictor/main.py
@@ -1,9 +1,14 @@
 """main of the package"""
 
+import argparse
 
-def main():
+
+def main(nbmax=None):
     pass
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Wenet activities predictor CLI")
+    parser.add_argument("--nbmax", help="number max of users", type=int, default=None)
+    args = parser.parse_args()
+    main(nbmax=args.nbmax)
